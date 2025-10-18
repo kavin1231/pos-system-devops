f9c689bff6f39a73691f2137d0ba474efcabf851
infofuturebound-afk <info.futurebound@gmail.com>
2025-10-18 10:56:44 +0530

    10

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -5,9 +5,9 @@ const CustomerController = require('../controller/CustomerController');
 const middleware = require('../middleware/Middleware');
 
 router.post('/create',middleware, CustomerController.createCustomer);
-router.post('/update/:id', CustomerController.updateCustomer);
-router.post('/delete/:id', CustomerController.deleteCustomer);
-router.post('/find-by-id/:id', CustomerController.findCustomerById);
-router.post('/load-all', CustomerController.loadAllCustomers);
+router.put('/update/:id', CustomerController.updateCustomer);
+router.delete('/delete/:id', CustomerController.deleteCustomer);
+router.get('/find-by-id/:id', CustomerController.findCustomerById);
+router.get('/load-all', CustomerController.loadAllCustomers);
 
 module.exports= router;
