270df6528454a1a4e2d55e03ae455c1d3924b127
infofuturebound-afk <info.futurebound@gmail.com>
2025-10-18 09:51:53 +0530

    4

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,6 +2,7 @@ const User = require('../model/UserSchema');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const JWT_SECERET = process.env.JWT_SECERET;
 
 const signup = async (req, resp) => {
     try {
@@ -11,7 +12,7 @@ const signup = async (req, resp) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const savedUser = await User.create({fullName, email, hashedPassword});
-        resp.status(201).json({'message':'User Created Successfully', data:savedUser});
+        resp.status(201).json({'message': 'User Created Successfully', data: savedUser});
 
     } catch (e) {
         resp.status(500).json({'message': 'Signup Error', error: e});
@@ -19,9 +20,18 @@ const signup = async (req, resp) => {
 };
 const login = async (req, resp) => {
     try {
+        const {email, password} = req.body;
+        const existingUser = await User.findOne({email});
+        if (!existingUser) return resp.status(404).json({'message': 'User Not Found'});
+
+        const isPasswordValid = await bcrypt.compare(password, existingUser.passwordHash);
+        if (!isPasswordValid) return resp.status(401).json({'message': 'Invalid Password'});
+
+        const token = jwt.sign({email:existingUser.email}, JWT_SECERET, {expiresIn: '10h'});
+        resp.status(200).json({'message': 'Success', token:token});
 
     } catch (e) {
         resp.status(500).json({'message': 'Signup Error', error: e});
     }
 };
-module.exports={signup,login};
+module.exports = {signup, login};
