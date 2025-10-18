b015c4d6b3b613cb3b2fac3c5b56e4d9690fc23f
infofuturebound-afk <info.futurebound@gmail.com>
2025-10-18 09:43:13 +0530

    3

--- controller/UserController.js
+++ controller/UserController.js
@@ -3,19 +3,25 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
-const signup = async (req,resp)=>{
-    try{
-        const {fullName, email, passwordHash}= req.body;
+const signup = async (req, resp) => {
+    try {
+        const {fullName, email, password} = req.body;
         const existingUser = await User.findOne({email});
-        if(existingUser) return resp.status(409).json({'message':'User Already exists'});
-    }catch (e) {
-        resp.status(500).json({'message':'Signup Error',error:e});
-    }
-};
-const login = async (req,resp)=>{
-    try{
+        if (existingUser) return resp.status(409).json({'message': 'User Already exists'});
 
-    }catch (e) {
-        resp.status(500).json({'message':'Signup Error',error:e});
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const savedUser = await User.create({fullName, email, hashedPassword});
+        resp.status(201).json({'message':'User Created Successfully', data:savedUser});
+
+    } catch (e) {
+        resp.status(500).json({'message': 'Signup Error', error: e});
     }
 };
+const login = async (req, resp) => {
+    try {
+
+    } catch (e) {
+        resp.status(500).json({'message': 'Signup Error', error: e});
+    }
+};
+module.exports={signup,login};
