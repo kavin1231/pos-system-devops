11a0cf1a774df7fb0be280208f22eb8238946bc3
infofuturebound-afk <info.futurebound@gmail.com>
2025-10-18 08:53:37 +0530

    1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+require('dotenv').config({ path: '.env', quiet: true });
 const mongoose = require('mongoose');
 
 const app = express();
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded);
 app.use(bodyParser.json());
 
 const PORT = process.env.SERVER_PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pos_system_devops';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://educationseekerscloud_db_user:seeo4zRp3ioGLrBC@cluster0.10yknbw.mongodb.net/pos_system_devops?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(MONGO_URI).then(()=>{
     console.log('Mongo db connected...');
