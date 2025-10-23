9b0c85ff8441983e7f233c5f40c07d350f4b69eb
kavin1231 <chamathkaveendra@gmail.com>
2025-10-23 17:08:10 +0530

    add sign up

--- controller/UserController.js
+++ controller/UserController.js
@@ -4,10 +4,13 @@ const jwt = require('jsonwebtoken');
 
 const signup async (req,resp)=>{
    try{
-      const{fullName,email,passwordHash}=req.body;
+      const{fullName,email,password}=req.body;
       const existingUser = await User.findOne({email});
       if(existingUser) return resp.status(409).json({'message':'User Already exists'});
 
+      const hashedPassword = await bcrypt.hash(password,10);
+      const savedUser = await User.create({fullName,email,hashedPassword});
+      resp.status(201).json({'message':'User Created Successfully', data:savedUser});
    }catch(e){
       resp.status(500).json({'message':'Signup error :',error:e})
    }
@@ -21,3 +24,5 @@ const login async (req,res)=>{
       resp.status(500).json({'message':'Signup error :',error:e})
    }
 };
+
+module.exports=(signup,login);
