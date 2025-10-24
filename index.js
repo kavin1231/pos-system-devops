3d2bb07b89ae7b349a1cb38135644b8781feea26
kavin1231 <chamathkaveendra@gmail.com>
2025-10-24 15:41:13 +0530

    done

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(bodyParser.json());
 
 app.use(cors())
 
-const UserRouter = require('./routes/UserRouter');
-const CustomerRouter = require('./routes/CustomerRouter');
-const ProductRouter = require('./routes/ProductRouter');
-const OrderRouter = require('./routes/OrderRouter');
+const UserRoute = require('./routes/UserRouter');
+const CustomerRoute = require('./routes/CustomerRouter');
+const ProductRoute = require('./routes/ProductRouter');
+const OrderRoute = require('./routes/OrderRouter');
 
 const PORT = process.env.SERVER_PORT || 3010;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://chamathkaveendra_db_user:r1TkZxaXHn4ePt2y@devops.2i9li4h.mongodb.net/pos_system_devops?retryWrites=true&w=majority&appName=Devops';
@@ -32,8 +32,7 @@ app.get('/test', (req,resp)=>{
     return resp.json({'message':'Server Stated..'});
 });
 
-app.use('/api/v1/users', UserRouter); // http://localhost:3000/api/v1/users/signup
-app.use('/api/v1/customers', CustomerRouter);
-app.use('/api/v1/products', ProductRouter);
-app.use('/api/v1/orders', OrderRouter);
-app.use('/api/v1/orders', OrderRouter);
+app.use('/api/v1/users', UserRoute); // http://localhost:3000/api/v1/users/signup
+app.use('/api/v1/customers', CustomerRoute);
+app.use('/api/v1/products', ProductRoute);
+app.use('/api/v1/orders', OrderRoute);
--- model/UserSchema.js
+++ model/UserSchema.js
@@ -1,18 +1,8 @@
 const mongoose = require('mongoose');
-
 const UserSchema = new mongoose.Schema({
-   fullName:{
-     type:String,
-     required:true
-   },
-   email:{
-      type:String,
-      required:true
-   },
-   passwordHash:{
-      type:String,
-      required:true
-   }
+    fullName:{type:String, required:true},
+    email:{type:String, required:true},
+    passwordHash:{type:String, required:true}
 });
 
-module.exports = mongoose.model('User',UserSchema);
+module.exports = mongoose.model('User', UserSchema);
