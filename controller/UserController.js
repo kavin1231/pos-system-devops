7ada868b3a64b53796c0c78ee6436fd431b22517
linuxcode <chamathkaveendra@gmail.com>
2025-12-11 06:14:50 +0000

    create product schema

---
+++ controller/UserController.js
@@ -0,0 +1,28 @@
+const User = require('../model/UserSchema');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const signup = async(req,resp)=>{
+
+   try{
+    const {fullName, email, passwordHash}= req.body;
+    const existingUser = await User.findOne({email});
+    if(existingUser) return resp.status(409).json({'message':'User Already Exists'});
+   }catch(e){
+       res.status(500).json({'message':'Signup Error',error:e});
+   }
+
+
+}
+
+const login = async(req,resp)=>{
+
+   try{
+
+   }catch(e){
+       res.status(500).json({'message':'Login Error',error:e});
+   }
+
+
+}
