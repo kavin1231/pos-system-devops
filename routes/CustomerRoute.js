a8c2d28e64b6923cfe70b22a94264763db3bbe3d
linuxcode <chamathkaveendra@gmail.com>
2025-12-11 10:40:49 +0000

    add user and customer routers

---
+++ routes/CustomerRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router =express.Router();
+
+const UserController = require('../controller/CustomerController');
+const middleware = require('../middleware/Middleware');
+
+
+// Create a new customer
+router.post('/create', CustomerController.createCustomer);
+
+// Update a customer by ID
+router.put('/update/:id', CustomerController.updateCustomer);
+
+// Delete a customer by ID
+router.delete('/delete/:id', CustomerController.deleteCustomer);
+
+// Find a customer by ID
+router.get('/find-by-id/:id', CustomerController.findCustomerById);
+
+// Load all customers
+router.get('/load-all', CustomerController.loadAllCustomers);
+
+module.exports= router;
